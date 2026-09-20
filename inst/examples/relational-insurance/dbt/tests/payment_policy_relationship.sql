a2e54a0a67c2abee955777d19c15019b96f587c1
Jan-Hendrik Weinert <65009403+JanWein@users.noreply.github.com>
2026-09-20 12:13:01 +0200

    test: reconcile dbt models against managed inputs

--- inst/examples/relational-insurance/dbt/tests/payment_policy_relationship.sql
+++ inst/examples/relational-insurance/dbt/tests/payment_policy_relationship.sql
@@ -1,6 +1,6 @@
 select payments.payment_id
-from {{ source('accepted_products', 'payments') }} as payments
-left join {{ source('accepted_products', 'policies') }} as policies
+from {{ source('inputs', 'payments') }} as payments
+left join {{ source('inputs', 'policies') }} as policies
   using (policy_id, month)
 where policies.policy_id is null
    or payments.company <> policies.company
