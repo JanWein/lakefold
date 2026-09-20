15190dcbdbf693d1197c874a130cc72eca60d5ae
Jan-Hendrik Weinert <65009403+JanWein@users.noreply.github.com>
2026-09-20 12:12:29 +0200

    refactor: bind insurance models to logical inputs

--- inst/examples/relational-insurance/dbt/models/core/core_policy_monthly.sql
+++ inst/examples/relational-insurance/dbt/models/core/core_policy_monthly.sql
@@ -5,5 +5,5 @@ select
   month,
   cast(sum(case when status = 'active' then 1 else 0 end) as integer) as active_policies,
   cast(sum(premium_due) as double) as premium_due
-from {{ source('accepted_products', 'policies') }}
+from {{ source('inputs', 'policies') }}
 group by company, channel, month
